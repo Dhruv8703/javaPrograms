1a9b2913c18e7c13fa445a9aade55e164861dcf0
Dhruv tewari <144560039+Dhruv8703@users.noreply.github.com>
2023-10-25 01:33:31 +0530

    Update ArithmeticOperatorsEx3.java

--- ArithmeticOperatorsEx3.java
+++ ArithmeticOperatorsEx3.java
@@ -2,7 +2,7 @@
 public class ArithmeticOperatorsEx3 {
 
 	public static void main(String[] args) {
-		int a= 8*3/2*6+9/4*2-8; // from left to right *,/,% have same priority thrn +,-
+		int a= 8*3/2*6+9/4*2-8; // from left to right *,/,% have same priority then +,-
 		System.out.println(a);
 		
 	int	b= 5/3*9%45*5+9*7/3;
